8af4471d8679425e30565c89441eebe6a635572e
yjtay <yjtay@axrail.com>
2023-06-25 17:41:04 +0800

    Added first board version

--- pages/index.js
+++ pages/index.js
@@ -7,10 +7,80 @@ export default function Home() {
   const [board, setBoard] = useState([]);
   const [flippedItems, setFlippedItems] = useState([]);
   const [matchedItems, setMatchedItems] = useState([]);
-  const [moves, setMoves] = useState(0);
+  const [flips, setFlips] = useState(0);
   const [gameOver, setGameOver] = useState(false);
   const nums1To8 = [...Array(8).keys()].map((i) => i + 1);
   const nums1To18 = [...Array(18).keys()].map((i) => i + 1);
+
+  const Board = () => {
+    return (
+      <div className="board">
+        {board.map((data, i) => {
+          const flipped = flippedItems.includes(i);
+          const matched = matchedItems.includes(i);
+          return (
+            <div
+              key={i}
+              className={`card ${flipped || matched ? "active" : ""} ${
+                matched ? "matched" : ""
+              } ${gameOver ? "gameover" : ""}`}
+              onClick={() => onFlip(i)}
+            >
+              <div className="card-front">{data}</div>
+              <div className="card-back" />
+            </div>
+          );
+        })}
+      </div>
+    );
+  };
+
+  const shuffle = () => {
+    const newBoard = [...nums1To8, ...nums1To8]
+      .sort(() => Math.random() - 0.5)
+      .map((v) => v);
+    setBoard(newBoard);
+  };
+
+  const initialize = () => {
+    shuffle();
+    setGameOver(false);
+    setFlippedItems([]);
+    setMatchedItems([]);
+    setFlips(0);
+  };
+
+  // handler function when user chooses item to flip
+  const onFlip = (i) => {
+    // check if the current item is not matched or flipped yet
+    if (!matchedItems.includes(i) && !flippedItems.includes(i)) {
+      // check how many items have been flipped on the board currently
+      if (flippedItems.length === 1) {
+        // check if the current chosen item is the same as the other flipped item on the board
+        if (board[i] === board[flippedItems[0]]) {
+          setMatchedItems((prev) => [...prev, i, flippedItems[0]]);
+        }
+
+        setFlippedItems([...flippedItems, i]);
+      } else if (flippedItems.length === 2) {
+        setFlippedItems([i]);
+      } else {
+        setFlippedItems([...flippedItems, i]);
+      }
+
+      setFlips((prev) => prev + 1);
+    }
+  };
+
+  useEffect(() => {
+    if (matchedItems.length === 16) {
+      setGameOver(true);
+    }
+  }, [flips]);
+
+  useEffect(() => {
+    initialize();
+  }, []);
 
   return (
     <>
@@ -22,8 +92,8 @@ export default function Home() {
       </Head>
       <main className={styles.main}>
         <nav>memory</nav>
-        <div>board</div>
-        <div>footer</div>
+        <Board />
+        <div>footer, moves: {Math.floor(flips / 2)}</div>
       </main>
     </>
   );
